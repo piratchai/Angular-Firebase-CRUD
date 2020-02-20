a4716f53e52d1bb52309501c4ac41e438ee639f5
GEC\piratchai.kot <piratchai.kot@gec.co.th>
2020-02-20 16:10:39 +0700

    เพิ่ม spinner loading และ update code.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import 'firebase/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -9,43 +10,38 @@ import 'firebase/firestore';
 })
 export class HomeComponent implements OnInit {
 
-  public studentCollection: any[];
-
-  public list: any[];
+  public studentCollection: Students[];
 
   public ID: string;
-  public Name: string;
-
   public Firstname: string;
   public Lastname: string;
   public ClassRoom: string;
 
+  public isEditMode = false;
+  public isLoading = false ;
+
   constructor(private firestore: AngularFirestore) {
-      this.initData();
 
-      // this.list = [
-      //     {
-      //       id: 1,
-      //       name: 'One'
-      //     },
-      //     {
-      //       id: 2,
-      //       name: 'Two'
-      //     }
-      // ];
-
-      // console.log(this.list);
   }
 
-  async initData(){
-    let students: any;
+  async initData() {
+    let students: Students[];
 
+    this.isLoading = true;
     await this.promiseData().then(value => {
-            students = value;
+            students = value as Students[];
     });
+
+    // this.ObservData().subscribe({
+    //   next(data) {
+    //       students = data as Students[];
+    //       console.log(data);
+    //   }
+    // });
 
     this.studentCollection = students;
     this.ID = this.studentCollection[this.studentCollection.length - 1].id + 1;
+    this.isLoading = false;
   }
 
   promiseData(){
@@ -56,21 +52,20 @@ export class HomeComponent implements OnInit {
     });
   }
 
-  ngOnInit() {
-
+  ObservData(){
+    return new Observable((obserable) => {
+        this.firestore.collection('students', r => r.orderBy('id', 'asc')).valueChanges().subscribe(value => {
+          obserable.next(value);
+        });
+    });
   }
 
-  // addList() {
-  //   const data = {
-  //       id: this.ID,
-  //       name: this.Name
-  //   };
-  //   this.list.push(data);
-
-  //   console.log(this.list);
-  // }
+  ngOnInit() {
+    this.initData();
+  }
 
   async addStudent() {
+    this.isLoading = true;
     const data = {
       id: this.ID,
       firstname: this.Firstname,
@@ -80,9 +75,52 @@ export class HomeComponent implements OnInit {
     await this.firestore.collection('students').doc(String(this.ID)).set(data);
 
     await this.initData();
+    this.clearFields();
+    this.isLoading = false;
+  }
 
+  async updateStudnet() {
+    this.isLoading = true;
+    const data = {
+      id: this.ID,
+      firstname: this.Firstname,
+      lastname: this.Lastname,
+      classRoom: this.ClassRoom
+    };
+    await this.firestore.collection('students').doc(String(this.ID)).set(data);
+
+    this.initData();
+    this.clearFields();
+
+    this.isEditMode = false;
+    this.isLoading = false;
+  }
+
+  receiveStudent(id: string) {
+      const student = this.studentCollection.find(element => {
+          return element.id === id;
+      });
+
+      this.isEditMode = true;
+      // console.log(student);
+
+      this.ID = student.id;
+      this.Firstname = student.firstname;
+      this.Lastname = student.lastname;
+      this.ClassRoom = student.classRoom;
+  }
+
+
+  clearFields() {
     this.Firstname = '';
     this.Lastname = '';
     this.ClassRoom = '';
   }
+}
+
+class Students {
+  public id: string;
+  public firstname: string;
+  public lastname: string;
+  public classRoom: string;
 }
